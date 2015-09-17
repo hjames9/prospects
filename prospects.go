8d668d832a588f254dd6a6ca5d0eadd720d4505c
Hayden A. James <hayden.james@gmail.com>
2015-09-16 23:12:39 -0400

    Added support for age, gender, language, http user agent, http cookies, geo location, source ip address, and miscellaneous json for unmodeled data.

--- prospects.go
+++ prospects.go
@@ -12,23 +12,33 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"strings"
 	"time"
 )
 
 const (
-	QUERY       = "INSERT INTO prospects (app_name, email, referrer, first_name, last_name, phone_number, created_at) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id;"
+	QUERY       = "INSERT INTO prospects (app_name, email, referrer, first_name, last_name, phone_number, age, gender, language, user_agent, cookies, geolocation, ip_address, miscellaneous, created_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, ARRAY[$11], POINT($12, $13), $14, $15, $16) RETURNING id;"
 	EMAIL_REGEX = "([\\w\\d\\.]+)@[\\w\\d\\.]+"
 	POST_URL    = "/prospects"
 	DB_DRIVER   = "postgres"
 )
 
 type ProspectForm struct {
-	AppName     string `form:"appname" binding:"required"`
-	Referrer    string `form:"referrer"`
-	FirstName   string `form:"firstname"`
-	LastName    string `form:"lastname"`
-	Email       string `form:"email" binding:"required"`
-	PhoneNumber string `form:"phonenumber"`
+	AppName       string `form:"appname" binding:"required"`
+	Referrer      string
+	FirstName     string `form:"firstname"`
+	LastName      string `form:"lastname"`
+	Email         string `form:"email" binding:"required"`
+	PhoneNumber   string `form:"phonenumber"`
+	Age           int64  `form:"age"`
+	Gender        string `form:"gender"`
+	Language      string `form:"language"`
+	UserAgent     string
+	Cookies       string
+	Latitude      float64 `form:"latitude"`
+	Longitude     float64 `form:"longitude"`
+	IpAddress     string
+	Miscellaneous string `form:"miscellaneous"`
 }
 
 type Response struct {
@@ -36,7 +46,7 @@ type Response struct {
 	Message string
 }
 
-type CreateHandler func(http.ResponseWriter, ProspectForm) (int, string)
+type CreateHandler func(http.ResponseWriter, *http.Request, ProspectForm) (int, string)
 type NotFoundHandler func(http.ResponseWriter, *http.Request) (int, string)
 
 func GetenvWithDefault(envKey string, defaultVal string) string {
@@ -89,17 +99,29 @@ func main() {
 }
 
 func setupHttpHandlers(db *sql.DB, emailRegex *regexp.Regexp) (CreateHandler, NotFoundHandler) {
-	createHandler := func(res http.ResponseWriter, prospect ProspectForm) (int, string) {
+	createHandler := func(res http.ResponseWriter, req *http.Request, prospect ProspectForm) (int, string) {
+		prospect.IpAddress = strings.Split(req.RemoteAddr, ":")[0]
+		prospect.Referrer = req.Referer()
+		prospect.UserAgent = req.UserAgent()
+
+		var cookiesArr []string
+		for _, cookie := range req.Cookies() {
+			cookiesArr = append(cookiesArr, cookie.String())
+		}
+		prospect.Cookies = strings.Join(cookiesArr, ", ")
+
 		log.Printf("Received new prospect: %#v", prospect)
 
 		res.Header().Set("Content-Type", "application/json")
 		var response Response
 
 		if emailRegex.MatchString(prospect.Email) {
-			if nil != addProspect(db, prospect) {
+			err := addProspect(db, prospect)
+			if nil != err {
 				responseStr := fmt.Sprintf("Could not add prospect due to server error: e-mail %s", prospect.Email)
 				response = Response{http.StatusInternalServerError, responseStr}
 				log.Print(responseStr)
+				log.Print(err)
 			} else {
 				responseStr := fmt.Sprintf("Successfully added prospect. E-mail %s", prospect.Email)
 				response = Response{http.StatusCreated, responseStr}
@@ -148,8 +170,50 @@ func addProspect(db *sql.DB, prospect ProspectForm) error {
 		referrer = sql.NullString{prospect.Referrer, true}
 	}
 
+	var age sql.NullInt64
+	if prospect.Age != 0 {
+		age = sql.NullInt64{prospect.Age, true}
+	}
+
+	var gender sql.NullString
+	if len(prospect.Gender) != 0 {
+		gender = sql.NullString{prospect.Gender, true}
+	}
+
+	var language sql.NullString
+	if len(prospect.Language) != 0 {
+		language = sql.NullString{prospect.Language, true}
+	}
+
+	var userAgent sql.NullString
+	if len(prospect.UserAgent) != 0 {
+		userAgent = sql.NullString{prospect.UserAgent, true}
+	}
+
+	var latitude sql.NullFloat64
+	var longitude sql.NullFloat64
+	if prospect.Latitude != 0 && prospect.Longitude != 0 {
+		latitude = sql.NullFloat64{prospect.Latitude, true}
+		longitude = sql.NullFloat64{prospect.Longitude, true}
+	}
+
+	var ipAddress sql.NullString
+	if len(prospect.IpAddress) != 0 {
+		ipAddress = sql.NullString{prospect.IpAddress, true}
+	}
+
+	var miscellaneous sql.NullString
+	if len(prospect.Miscellaneous) != 0 {
+		miscellaneous = sql.NullString{prospect.Miscellaneous, true}
+	}
+
+	var cookies sql.NullString
+	if len(prospect.Cookies) != 0 {
+		cookies = sql.NullString{prospect.Cookies, true}
+	}
+
 	var lastInsertId int
-	err := db.QueryRow(QUERY, prospect.AppName, prospect.Email, referrer, firstName, lastName, phoneNumber, time.Now()).Scan(&lastInsertId)
+	err := db.QueryRow(QUERY, prospect.AppName, prospect.Email, referrer, firstName, lastName, phoneNumber, age, gender, language, userAgent, cookies, latitude, longitude, ipAddress, miscellaneous, time.Now()).Scan(&lastInsertId)
 
 	if nil == err {
 		log.Printf("New prospect id = %d", lastInsertId)
--- prospects.sql
+++ prospects.sql
@@ -1,3 +1,5 @@
+CREATE TYPE gender AS ENUM ('male', 'female');
+
 CREATE TABLE prospects
 (
     id SERIAL8 NOT NULL PRIMARY KEY,
@@ -7,6 +9,14 @@ CREATE TABLE prospects
     first_name VARCHAR NULL,
     last_name VARCHAR NULL,
     phone_number VARCHAR NULL,
+    age SMALLINT NULL,
+    gender GENDER NULL,
+    language VARCHAR NULL,
+    user_agent VARCHAR NULL,
+    cookies VARCHAR[] NULL,
+    geolocation POINT NULL,
+    ip_address INET NULL,
+    miscellaneous JSONB NULL,
     is_valid BOOLEAN NOT NULL DEFAULT FALSE,
     was_processed BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP NOT NULL
@@ -14,7 +24,7 @@ CREATE TABLE prospects
 
 CREATE VIEW sneezers
 AS
-SELECT MAX(id) AS id, app_name, email, MAX(first_name) AS first_name, MAX(last_name) AS last_name, MAX(phone_number) AS phone_number, MAX(created_at) AS created_at FROM prospects WHERE is_valid = TRUE AND was_processed = TRUE GROUP BY app_name, email;
+SELECT MAX(id) AS id, app_name, email, MAX(first_name) AS first_name, MAX(last_name) AS last_name, MAX(phone_number) AS phone_number, MAX(age) AS age, MAX(gender) AS gender, MAX(user_agent) AS user_agent, MAX(created_at) AS created_at FROM prospects WHERE is_valid = TRUE AND was_processed = TRUE GROUP BY app_name, email;
 
 CREATE INDEX p_app_name_idx ON prospects(app_name);
 
