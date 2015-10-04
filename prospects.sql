6bf2663e883b170671725f27c172cb9e7dbcabcd
Hayden A. James <hayden.james@gmail.com>
2015-10-04 19:34:12 -0400

    Add check constraints for age, email and geolocation.

--- prospects.sql
+++ prospects.sql
@@ -21,12 +21,29 @@ CREATE TABLE prospects
     miscellaneous JSONB NULL,
     is_valid BOOLEAN NOT NULL DEFAULT FALSE,
     was_processed BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP NOT NULL,
+    CHECK(email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'),
+    CHECK(age >= 0 AND age <= 200),
+    CHECK(geolocation.x >= -90.0 AND geolocation.x <= 90.0 AND geolocation.y >= -180.0 AND geolocation.y <= 180.0)
 );
 
 CREATE VIEW sneezers
 AS
-SELECT MAX(id) AS id, app_name, email, MAX(first_name) AS first_name, MAX(last_name) AS last_name, MAX(phone_number) AS phone_number, MAX(age) AS age, MAX(gender) AS gender, MAX(zip_code) AS zip_code, MAX(user_agent) AS user_agent, MAX(created_at) AS created_at FROM prospects WHERE is_valid = TRUE AND was_processed = TRUE GROUP BY app_name, email;
+SELECT MAX(id) AS id,
+       app_name,
+       email,
+       MAX(first_name) AS first_name,
+       MAX(last_name) AS last_name,
+       MAX(phone_number) AS phone_number,
+       MAX(age) AS age,
+       MAX(gender) AS gender,
+       MAX(zip_code) AS zip_code,
+       MAX(language) AS language,
+       MAX(user_agent) AS user_agent,
+       MAX(created_at) AS created_at
+FROM prospects
+WHERE is_valid = TRUE AND was_processed = TRUE
+GROUP BY app_name, email;
 
 CREATE INDEX p_app_name_idx ON prospects(app_name);
 
